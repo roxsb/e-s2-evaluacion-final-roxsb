4068632aed1fd46962e3405842d4b29871bd33ee
roxsb <rox.san.bri@gmail.com>
2018-12-17 20:41:09 +0100

    search images and draw it

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,16 +1,21 @@
 'use strict';
 
-//comprobar que funciona el js
-
-const pepi = document.querySelector('.page__title');
-pepi.innerHTML = 'Buscador de unicornios';
-
 //obtener el contenedor donde se pintará la lista
 const listResult = document.querySelector('.list__results');
+//obtener el elemento botón y añadirle un listener
+const button = document.querySelector('.btn');
+
+const handleButton = () => {
+  //recoger la palabra escrita por el usuario
+  const userSearch = document.querySelector('.input__name').value;
+  search(userSearch);
+};
+
+button.addEventListener('click', handleButton);
 
 //llamar a la API y crear una función para hacer la llamada a la Api según la búsqueda
-
 function search (searchName){
+  listResult.innerHTML='';
 
   fetch('http://api.tvmaze.com/search/shows?q=' + searchName)
     .then(function(response) {
@@ -18,46 +23,17 @@ function search (searchName){
     })
     .then((seriesResponse) => {
       //Por cada elemento encontrado pintar una tarjeta
-      for (const x of seriesResponse) {
-        listResult.innerHTML +=
-        `<li class="list__element">
-          <h2 class="serie__title">${x.show.name}</h2>
-          <img class="serie__image" src="${x.show.image}" alt="${x.show.title}">
-        </li>`;
-      }
+      draw(seriesResponse);
     });
-
 }
 
-//obtener el elemento botón y añadirle un listener
-const button = document.querySelector('.btn');
-const handleButton = () => {
-  //recoger la palabra escrita por el usuario
-  const userSearch = document.querySelector('.input__name').value;
-  console.log(userSearch);
-  search(userSearch);
-};
-
-button.addEventListener('click', handleButton);
-
-// .then((seriesData) => {
-  //   //añadir la palabra buscada a la url
-  //   const data = seriesData.message;
-  //   return fetch('http://api.tvmaze.com/search/shows?q=' + data[0] + 'http://static.tvmaze.com/uploads/images/medium_portrait/');
-  // });
-
-// const button = document.querySelector('.btn')[0];
-// button.addEventListener('click',function(event){
-//     //obtener el nombre introducido por la usuaria
-//     const userSearch = parseInt(document.querySelector('.input__name')[0].innerHTML);
-//     const listElement = document.querySelector('.list__element');
-
-//     //muestra las comprobaciones
-//     if(x.name.includes(userSearch)  ){
-//       let result = 'estas son las series con la palabra ${name}';
-//       listElement.innerHTML = result; //muestra las series resultado de la comprobación
-
-//     }else{
-//   }
-// });
-
+//pintar los resultados obtenidos según la búsqueda
+function draw (seriesResponse){
+  for (const x of seriesResponse) {
+    listResult.innerHTML +=
+    `<li class="list__element">
+      <h2 class="serie__title">${x.show.name}</h2>
+      <img class="serie__image" src="${x.show.image.medium}" alt="${x.show.name}">
+    </li>`;
+  }
+}
